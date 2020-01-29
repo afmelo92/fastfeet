dead4f3afca3e7df2b9ceb0c9dfb2c7342fefb54
afmelo92 <andre.fabian.melo@gmail.com>
2020-01-29 00:11:30 -0400

    Add Recipient store/update

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,81 @@
+import * as Yup from 'yup';
+
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip: Yup.string()
+        .matches(/^[0-9]{5}(?:-[0-9]{3})?$/, 'Must be in zip format')
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip,
+    } = await Recipient.create(req.body);
+
+    return res.json({ name, street, number, complement, state, city, zip });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip: Yup.string()
+        .matches(/^[0-9]{5}(?:-[0-9]{3})?$/, 'Must be in zip format')
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipient = await Recipient.findByPk(req.params.recipientId);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipient does not exists' });
+    }
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      zip,
+    });
+  }
+}
+
+export default new RecipientController();
